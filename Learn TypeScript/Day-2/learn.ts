024e0af7a5a260f48defd5a918d4041afd04474b
MD AJIM <mdajim2669@gmail.com>
2025-08-21 14:02:32 +0000

    learn day 2

--- Learn TypeScript/Day-2/learn.ts
+++ Learn TypeScript/Day-2/learn.ts
@@ -1,17 +1,38 @@
 
 //Type annotation means stating the type of the variable in advance.
 
-let value : string = 'Hello'
+
+let value: string = 'Hello, TypeScript!';
 
 let variableName: string = value;
 
 //Basic Data Types of TypeScript
 
 let age : number = 20
-
-
 let Name : string = 'Ajim'
+let isDeveloper : boolean = true
 
-console.log(Name , 'name')
 
-console.log(age, 'age')
+//Any (can take any type)
+
+let data : any = 'Hello'
+data = 123
+
+let arr: any[] = [1, 'Hello', true, { key: 'value' }];
+arr.push(42);
+arr.push('World');
+arr.push(false);
+arr.push({ newKey: 'newValue' }); 
+
+//But using any too much reduces the benefits of TypeScript.
+//Unknown (safe any)
+
+let input: unknown = 'Hi';
+input = 42;
+// console.log(input.toUpperCase()); ❌ Error (because not sure if input is string or not)
+
+
+
+
+
+
