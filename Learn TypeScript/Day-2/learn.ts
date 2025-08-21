1af49d2a82b142b49ce51985efb7d1289761305b
MD AJIM <mdajim2669@gmail.com>
2025-08-21 12:33:45 +0000

    learn day 2

---
+++ Learn TypeScript/Day-2/learn.ts
@@ -0,0 +1,17 @@
+
+//Type annotation means stating the type of the variable in advance.
+
+let value : string = 'Hello'
+
+let variableName: string = value;
+
+//Basic Data Types of TypeScript
+
+let age : number = 20
+
+
+let Name : string = 'Ajim'
+
+console.log(Name , 'name')
+
+console.log(age, 'age')
