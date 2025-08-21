cbe20c2ceeac9bb73b58e2f93000b826615de021
MD AJIM <mdajim2669@gmail.com>
2025-08-21 11:49:54 +0000

    practice day 1

--- Learn TypeScript/Day-1/learn.ts
+++ Learn TypeScript/Day-1/learn.ts
@@ -1,2 +1,11 @@
-let message : string = 'Hello TypeScript'
+let message: string = "Hello, TypeScript!";
+let bol : boolean = true
+let number : number = 50
+
+
+console.log(message);
+console.log(bol, 'bool')
+console.log(number, 'number')
+
+
 
