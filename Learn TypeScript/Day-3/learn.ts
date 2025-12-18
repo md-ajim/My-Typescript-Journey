217b3c92d197ae40afc6f5d888281e1a758df50e
md-ajim <mdajim2669@gmail.com>
2025-12-18 19:15:05 +0600

    Add initial variable declarations in learn.ts
    Introduced three variables: Name (string), age (number), and debeloper (boolean). This sets up the basic data structure for further development.

--- Learn TypeScript/Day-3/learn.ts
+++ Learn TypeScript/Day-3/learn.ts
@@ -5,3 +5,17 @@ let age : number = 28;
 let debeloper : boolean = true;
 
 
+// Type Inference
+
+
+let address : string = 'Dhaka' // automatically string
+
+
+// Unione Type
+
+
+let status : 'loading' | 'success' | 'errors'
+
+status = 'loading'
+
+console.log(status, 'status')
